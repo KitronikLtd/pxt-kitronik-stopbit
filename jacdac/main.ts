ccd847d2ebddcaf12b632b4b2c39834d4d0a336e
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-04-07 21:38:54 -0700

    Update jacdac/pxt.json, jacdac/main.ts

---
+++ jacdac/main.ts
@@ -0,0 +1,39 @@
+// % deprecated
+namespace Kitronik_STOPbit {
+
+}
+
+namespace servers {
+    function toDisplayLights(b: boolean): Kitronik_STOPbit.DisplayLights {
+        return b ? Kitronik_STOPbit.DisplayLights.On : Kitronik_STOPbit.DisplayLights.Off
+    }
+    class TrafficLightServer extends jacdac.Server {
+        private red: boolean
+        private green: boolean
+        private yellow: boolean
+
+        constructor() {
+            super("", jacdac.SRV_TRAFFIC_LIGHT)
+        }
+
+        handlePacket(pkt: jacdac.JDPacket) {
+            this.red = this.handleRegBool(pkt, jacdac.TrafficLightReg.Red, this.red)
+            this.green = this.handleRegBool(pkt, jacdac.TrafficLightReg.Green, this.green)
+            this.yellow = this.handleRegBool(pkt, jacdac.TrafficLightReg.Yellow, this.yellow)
+
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, toDisplayLights(this.red))
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, toDisplayLights(this.red))
+            Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, toDisplayLights(this.yellow))
+        }
+    }
+
+    jacdac.startSelfServers(() => [new TrafficLightServer()])
+}
+
+namespace modules {
+    /**
+     * Traffic light controller for the Kitronik Stopbit lights
+     */
+    //% fixedInstance
+    export const KitronikStopbitLights = new TrafficLightClient("kitronik stopbit lights?device=self")
+}
